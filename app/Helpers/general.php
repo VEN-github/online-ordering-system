ca95604acd980dffefe728a7c1f77f183acf3626
jade-creator <jadelestersoriano@gmail.com>
2023-04-07 16:44:43 +0800

    :sparkles: Helper(general): added parameter option for uppercase version.
    Signed-off-by: Jade Soriano

--- app/Helpers/general.php
+++ app/Helpers/general.php
@@ -11,8 +11,8 @@ if (! function_exists('include_files_in_directory')) {
 }
 
 if (! function_exists('get_latest_api_version_with_prefix')) {
-    function get_latest_api_version_with_prefix(): string
+    function get_latest_api_version_with_prefix($isUppercase = true): string
     {
-        return 'V' . config('api.version.latest');
+        return $isUppercase ? 'V' : 'v' . config('api.version.latest');
     }
 }
