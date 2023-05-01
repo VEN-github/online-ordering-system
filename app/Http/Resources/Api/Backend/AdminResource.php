f039fdd3cf3aefeab0e6604db28baa7f09d76015
jade-creator <jadelestersoriano@gmail.com>
2023-05-01 09:47:46 +0800

    :construction: git(merge): fix conflicts
    Signed-off-by: Jade Soriano :sparkles:

--- app/Http/Resources/Api/Backend/AdminResource.php
+++ app/Http/Resources/Api/Backend/AdminResource.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Http\Resources;
+namespace App\Http\Resources\Api\Backend;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
--- app/Http/Resources/Api/Backend/ProductResource.php
+++ app/Http/Resources/Api/Backend/ProductResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources\Api\Backend;
 
 use App\Collections\AttributeCollection;
 use App\Collections\VariationCollection;
+use App\Http\Resources\SupplierResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
