bca0f534fa1d3260d2c6be1c895f3a1d20d0922f
jade-creator <jadelestersoriano@gmail.com>
2023-05-01 09:54:01 +0800

    :fire: Resource(product): removed supplier resource import namespace
    Signed-off-by: Jade Soriano :sparkles:

--- app/Http/Resources/Api/Backend/ProductResource.php
+++ app/Http/Resources/Api/Backend/ProductResource.php
@@ -4,7 +4,6 @@ namespace App\Http\Resources\Api\Backend;
 
 use App\Collections\AttributeCollection;
 use App\Collections\VariationCollection;
-use App\Http\Resources\SupplierResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
