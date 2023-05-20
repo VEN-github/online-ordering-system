a8782a00ff2e6f248c82c973538ec0ebb56ced68
jade-creator <jadelestersoriano@gmail.com>
2023-05-20 19:31:46 +0800

    :sparkles: endpoint for non paginated category index
    Signed-off-by: Jade Soriano :sparkles:

---
+++ app/Http/Controllers/Api/V1/Backend/Product/CategoryController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Backend\Product;
+
+use App\Http\Controllers\Api\BaseController;
+use App\Http\Resources\Api\Backend\CategoryResource;
+use App\Models\Category\Category;
+use Exception;
+
+class CategoryController extends BaseController
+{
+    public function index()
+    {
+        try {
+            $categories = Category::query()
+                ->latest()
+                ->get();
+
+            return $this->success(
+                config('general.messages.request.success'),
+                CategoryResource::collection($categories)
+            );
+        } catch (Exception $e) {
+            return $this->error();
+        }
+    }
+}
--- app/Http/Resources/Api/Backend/CategoryResource.php
+++ app/Http/Resources/Api/Backend/CategoryResource.php
@@ -15,6 +15,7 @@ class CategoryResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'slug'=> $this->slug,
             'created_at' => $this->created_at,
