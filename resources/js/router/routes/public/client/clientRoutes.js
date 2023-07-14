89946849917d2451571bd381e13196fdfb4fdc92
Raven Barrogo <barrogoraven@gmail.com>
2023-07-15 02:15:58 +0800

    FEATURE: added client routes

--- resources/js/router/routes/public/client/clientRoutes.js
+++ resources/js/router/routes/public/client/clientRoutes.js
@@ -1,15 +1,44 @@
-import HomeView from '@/views/client/HomeView.vue'
+import ClientLayout from '@/layouts/client/ClientLayout.vue'
 
 const routes = [
   {
-    path: '/',
+    path: '/home',
     name: 'home',
-    component: HomeView
+    component: ClientLayout,
+    redirect: '/',
+    children: [
+      {
+        path: '/',
+        component: () => import('@/views/client/HomeView.vue'),
+        meta: {
+          title: 'Home'
+        }
+      },
+      {
+        path: '/about',
+        name: 'about',
+        component: () => import('@/views/client/AboutView.vue'),
+        meta: {
+          title: 'About'
+        }
+      },
+      {
+        path: '/contact',
+        name: 'contact',
+        component: () => import('@/views/client/ContactView.vue'),
+        meta: {
+          title: 'Contact'
+        }
+      }
+    ]
   },
   {
-    path: '/about',
-    name: 'about',
-    component: () => import('@/views/client/AboutView.vue')
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/client/LoginView.vue'),
+    meta: {
+      title: 'Login'
+    }
   }
 ]
 
