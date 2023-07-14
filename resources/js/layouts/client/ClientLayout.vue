449b5cde928d13b07f18f19650463188685076de
Raven Barrogo <barrogoraven@gmail.com>
2023-07-15 02:18:52 +0800

    FEATURE: added client layout & contact & about page

---
+++ resources/js/layouts/client/ClientLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <TheHeader />
+  <RouterView />
+  <TheFooter />
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+
+import TheHeader from './TheHeader.vue'
+import TheFooter from './TheFooter.vue'
+</script>
