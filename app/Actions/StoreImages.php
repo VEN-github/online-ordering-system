92f6a2906a553856202eac1b6cb3162c83c9158e
Jade Soriano <jadelestersoriano@gmail.com>
2023-07-15 10:38:57 +0800

    store images temporarily then move to permanent folder

--- app/Actions/StoreImages.php
+++ app/Actions/StoreImages.php
@@ -4,6 +4,8 @@ namespace App\Actions;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 use Lorisleiva\Actions\Concerns\AsAction;
 
 class StoreImages
@@ -41,15 +43,24 @@ class StoreImages
 
         if ($this->request->hasFile($collection)) {
             if (is_array($this->request->file($collection))) {
-                $model
-                    ->addMultipleMediaFromRequest([$collection])
-                    ->each(function ($fileAdder) use ($collection) {
-                        $fileAdder->toMediaCollection($collection);
-                    });
+                foreach ($this->request->file($collection) as $file) {
+                    $path = $file->storeAs('public/images', uniqid() . '.jpg', 'local');
+
+                    $model
+                        ->addMedia($path)
+                        ->toMediaCollection($collection);
+
+                    // Storage::disk('local')->delete($path);
+                }
             } else {
+                $file = $this->request->file($collection);
+                $path = $file->storeAs('public/images', uniqid() . '.jpg', 'local');
+
                 $model
                     ->addMediaFromRequest($collection)
                     ->toMediaCollection($collection);
+
+                // Storage::disk('local')->delete($path);
             }
         }
     }
