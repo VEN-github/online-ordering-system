1843ea277b8e5f02de1dda025f8b1044cfdd5c9d
vagrant <jadelestersoriano@gmail.com>
2023-08-17 14:17:19 +0000

    Added update order status

--- app/Http/Controllers/Api/V1/Backend/Order/OrderController.php
+++ app/Http/Controllers/Api/V1/Backend/Order/OrderController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api\V1\Backend\Order;
 
 use App\Http\Controllers\Api\BaseController;
+use App\Http\Requests\Api\Frontend\Order\OrderRequest;
+use App\Http\Requests\Api\Frontend\Order\UpdateOrderStatusRequest;
 use App\Http\Resources\Api\Backend\OrderResource;
 use App\Models\Order\Order;
 use Illuminate\Http\Request;
@@ -21,6 +23,29 @@ class OrderController extends BaseController
                     config('general.messages.request.success'),
                     OrderResource::collection($orders)
                 );
+        } catch (\Exception $e) {
+            return $this->error($e->getMessage());
+        }
+    }
+
+    public function update(UpdateOrderStatusRequest $request, string $refId)
+    {
+        try {
+            $order = Order::query()
+                ->eagerLoadRelationships()
+                ->whereRefId($refId)
+                ->first();
+
+            if (is_null($order)) {
+                return $this->error(config('general.messages.model.not_found'));
+            }
+
+            $order->update(['status' => $request->status]);
+
+            return $this->success(
+                config('general.messages.model.updated'),
+                OrderResource::make($order)
+            );
         } catch (\Exception $e) {
             return $this->error($e->getMessage());
         }
--- routes/api/V1/Backend/Order/order.php
+++ routes/api/V1/Backend/Order/order.php
@@ -6,8 +6,8 @@ Route::middleware(['auth:api_admins'])
     ->name('order.')
     ->group(function() {
         Route::get('/', 'OrderController@index')->name('index');
-        Route::post('/', 'OrderController@store')->name('store');
-        Route::get('/{slug}', 'OrderController@show')->name('show');
-        Route::patch('/{slug}', 'OrderController@update')->name('update');
-        Route::delete('/{slug}', 'OrderController@destroy')->name('destroy');
+        // Route::post('/', 'OrderController@store')->name('store');
+        // Route::get('/{slug}', 'OrderController@show')->name('show');
+        Route::patch('/{refId}', 'OrderController@update')->name('update');
+        // Route::delete('/{slug}', 'OrderController@destroy')->name('destroy');
     });
