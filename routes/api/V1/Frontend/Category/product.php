49abb98827fb1634730725e09fc2591c3fc51ed8
Jade Soriano <jadelestersoriano@gmail.com>
2023-10-03 20:46:00 +0800

    fix route names
    Signed-off-by: Jade Soriano <jadelestersoriano@gmail.com>

--- routes/api/V1/Frontend/Category/product.php
+++ routes/api/V1/Frontend/Category/product.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 Route::prefix('category')
     ->namespace('Category')
-    ->name('category.')
+    ->name('category.product')
     ->group(function () {
         Route::get('{category}/products', 'ProductController@index')->name('index');
     });
