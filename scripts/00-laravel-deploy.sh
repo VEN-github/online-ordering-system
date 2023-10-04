07f13d84ab220a7cdc673c7da87f36cd3c042ba1
Jade Soriano <jadelestersoriano@gmail.com>
2023-10-04 23:48:53 +0800

    remove config cache
    Signed-off-by: Jade Soriano <jadelestersoriano@gmail.com>

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -5,9 +5,6 @@ composer install --working-dir=/var/www/html
 echo "Clearing caches..."
 php artisan optimize:clear
 
-echo "Caching config..."
-php artisan config:cache
-
 echo "Caching routes..."
 php artisan route:cache
 
