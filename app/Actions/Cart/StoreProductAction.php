7b84c28968c2cc7a1419b13aec644f09092fa4ad
Jade Soriano <jadelestersoriano@gmail.com>
2023-10-14 12:41:13 +0800

    refactor action
    Signed-off-by: Jade Soriano <jadelestersoriano@gmail.com>

--- app/Actions/Cart/StoreProductAction.php
+++ app/Actions/Cart/StoreProductAction.php
@@ -14,12 +14,15 @@ class StoreProductAction
     use AsAction;
 
     public function handle(
-        Cart $cart,
         Product $product,
         ?Variation $variation = null,
         int $quantity = 1,
-    ): mixed
+    ): Cart
     {
+        /** @var \App\Models\Cart\Cart $cart */
+        $cart = auth()->user()->cart;
+
+        /** @var \App\Models\Cart\CartProduct|null $existingProduct */
         $existingProduct = $cart->products()
             ->where('product_id', $product->id)
             ->when(
@@ -28,6 +31,7 @@ class StoreProductAction
             )
             ->first();
 
+        /** @var array<string, int> $cartProductData */
         $cartProductData = GetItemQuantityAndTotalPriceAction::run(
                 $existingProduct ?? $product,
                 $quantity
@@ -35,11 +39,13 @@ class StoreProductAction
             ->toArray();
 
         if ($existingProduct) {
-            return $existingProduct->update($cartProductData);
+            $existingProduct->update($cartProductData);
+        } else {
+            $cart->products()->create(
+                array_merge(['variation_id' => $variation->id], $cartProductData)
+            );
         }
 
-        return $cart->products()->create(
-            array_merge(['variation_id' => $variation->id], $cartProductData)
-        );
+        return $cart;
     }
 }
