613ab95a1aad2fce3be1f2008a57d4ad36ce61f5
Jade Soriano <jadelestersoriano@gmail.com>
2023-10-15 11:38:59 +0800

    app name
    Signed-off-by: Jade Soriano <jadelestersoriano@gmail.com>

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,7 +12,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-    <title>Online Ordering System</title>
+    <title>{{ config('app.name') }}</title>
 
     @vite('resources/css/app.css')
   </head>
