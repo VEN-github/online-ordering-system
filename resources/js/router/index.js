25c6c0e9617c756574b352be6898d01613f1fcfc
Jade Soriano <jadelestersoriano@gmail.com>
2024-01-20 22:33:10 +0800

    added root to history
    Signed-off-by: Jade Soriano <jadelestersoriano@gmail.com>

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -5,7 +5,7 @@ import publicRoutes from './routes/public/publicRoutes'
 import privateRoutes from './routes/private/privateRoutes'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL || '/'),
   routes: [
     ...publicRoutes,
     ...privateRoutes,
